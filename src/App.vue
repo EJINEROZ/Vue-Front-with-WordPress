d916b94ecf080edd59e003e22ddb67c41ca56a43
UWEGHREPHA EJIRO THANKGOD <ejineroz@pop-os.localdomain>
2022-10-02 12:54:09 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <ul v-for="post in posts" v-bind:key="post.id">
+      <li>
+        <h2>{{ post.title.rendered }}</h2>
+      </li>
+      <p>{{ post.content.rendered }}</p>
+    </ul>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch(
+          "http://localhost/wordpress/wp-json/wp/v2/posts"
+        );
+        this.posts = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
